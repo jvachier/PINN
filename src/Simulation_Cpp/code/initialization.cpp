87686c7c81c209442aef058edb03e0c233e25c47
Jeremy Vachier <89128100+jvachier@users.noreply.github.com>
2024-05-15 19:10:59 +0200

    cpplint initialization.cpp.

--- src/Simulation_Cpp/code/initialization.cpp
+++ src/Simulation_Cpp/code/initialization.cpp
@@ -3,15 +3,13 @@
 using namespace std;
 
 void initialization(
-	double *x, int Particles,
-	default_random_engine &generator, uniform_real_distribution<double> &distribution)
-{
-
-// Position
+  double *x, int Particles,
+  default_random_engine *generator,
+  uniform_real_distribution<double> *distribution) {
+  // Position
 #pragma omp parallel for simd
-	for (int k = 0; k < Particles; k++)
-	{
-		//x[k] = distribution(generator);
-		x[k] = 0.0;
-	}
+  for (int k = 0; k < Particles; k++) {
+  // x[k] = distribution(generator);
+  x[k] = 0.0;
+  }
 }
